b312fcf65dcd94a5be3df3a7c4b0ce821c0a06a0
sandip mali <malisandip9211@gmail.com>
2023-05-02 14:15:07 +0530

    For-while-loop

---
+++ src/test/scala/WAP_1to_100.scala
@@ -0,0 +1,14 @@
+object WAP_1to_100 {
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 to 100 )
+      {
+        println("Number is : " +i )
+      }
+    var j: Int = 1
+    while (j <= 100)
+      {
+        println("the Number Is : " + j)
+        j = j + 1
+      }
+  }
+}
